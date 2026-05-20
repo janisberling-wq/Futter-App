09162a32b4bb9ad0f32b6db1256ab2e35922484a
x <x>
2026-05-20 05:32:27 +0200

    Bump version to 1.0.1

--- app.config.ts
+++ app.config.ts
@@ -41,8 +41,8 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.0.0",
-  runtimeVersion: "1.0.0",
+  version: "1.0.1",
+  runtimeVersion: "1.0.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -55,6 +55,7 @@ const config: ExpoConfig = {
   },
   ios: {
     supportsTablet: true,
+    buildNumber: "2",
     bundleIdentifier: env.iosBundleId,
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
